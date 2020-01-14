6ac1f257cf45c728f330dac97c593eba4a72f5a6
Tianze Peng <tpeng2@nd.edu>
2020-01-14 14:27:53 -0500

    add Vpmax value (previously missing in the input file and reading subroutines)

--- params.f90
+++ params.f90
@@ -2,30 +2,33 @@
 !Time step information
 &step_params
 iti=0          !Time step to begin
-itmax=5000   !Max number of time steps
+itmax=10000   !Max number of time steps
 imean=100    !Number of time steps between writing mean profiles to output log file
-ihst=1     !Number of time steps between writing profiles to history file
-itape=50  !Number of time steps before opening new history file and writing full 3D volumes
+ihst=10      !Number of time steps between writing profiles to history file
+itape=5000  !Number of time steps before opening new history file and writing full 3D volumes
 itstr=1
 it_his=1     !Time step to begin recording history files
 it_viz=1     !Time step to begin recording viz files
 i_viz=1000   !Time steps between writing to viz file
 itn = 0  !Index for "u.le.cou001" files
-itcplbegin=100
+itcplbegin=001
+itclsbegin=001
 /
 
 !Flags for various features
 &flags
 ismlt=0, ifree=0, iradup=1, iupwnd=1
 ibuoy=1, ifilt=0, itcut=1, isubs=0, ibrcl=0, iocean=0
-method=3, idebug=1, iz_space=1, ivis0=1, ifix_dt=0, new_vis=-1
+method=3, idebug=0, iz_space=1, ivis0=1, ifix_dt=0, new_vis=-1
 isfc = 0, 0  ! isfc = 0 sets surface flux (qstar), isfc = 1 sets surface condition (Dirichlet through tsfcc)
 iDNS = 0
 ifields = 0  ! if set to 1, reads the initial condition from path_ran below; otherwise calls subroutine random
+isurfUlog=0 !0: constant lower boundary, 1: log profile for first 100 m
+itempfld=1 ! 0: horizontally homogeneous, 1: Gaussian bubble
 ilin = 0   !1 means linear interpolation for particle, 6th order otherwise
 icouple=0
-iTcouple=0
-iHcouple=0  !iHcouple also controls TE couple
+iTcouple=1
+iHcouple=1  !iHcouple also controls TE couple
 ievap=1
 ineighbor=0
 icoalesce=0
@@ -59,10 +62,9 @@ Ttop(1) = 300.15, 95.0  !Currently this should be (temperature, relative humidit
 Tbot(1) = 300.15, 100.0  !Currently this should be (temperature, relative humidity) (used in DNS)
 
 !Use for LES:
-qstar = 0.2, 0.2  !Surface fluxes of (temperature, humidity) (used for LES and DNS)
-tsfcc = 300.0, 100.0    !Surface conditions of (temp, humidity) (used for LES) -- make sure tsfcc is gt than t00 for both isfc=0 or 1
+qstar = 0.0, 0.0  !Surface fluxes of (temperature, humidity) (used for LES and DNS)
+tsfcc = 300.0, 098.0    !Surface conditions of (temp, humidity) (used for LES) -- make sure tsfcc is gt than t00 for both isfc=0 or 1
 
-surf_RH = 90.0
 
 psurf = 101325  !Pa -- nominal surface pressure, for use in the definition of potential temp
 
@@ -81,17 +83,17 @@ zw1 = 1.25  !The first grid point
 
 !Set the paths for code I/O. Must be on the scratch directory, not AFS!
 &path_names
-path_seed="/scratch365/tpeng2/NTLP-SD/ABLtest/"
-path_part="/scratch365/tpeng2/NTLP-SD/ABLtest/part.le.cou000"
-path_res="/scratch365/tpeng2/NTLP-SD/ABLtest/u.le.cou000"
-path_sav="/scratch365/tpeng2/NTLP-SD/ABLtest/"
-path_his="/scratch365/tpeng2/NTLP-SD/ABLtest/"
-path_viz_xy="/scratch365/tpeng2/NTLP-SD/ABLtest/"
-path_viz_xz="/scratch365/tpeng2/NTLP-SD/ABLtest/"
-path_viz_yz="/scratch365/tpeng2/NTLP-SD/ABLtest/"
-path_stuf="/scratch365/tpeng2/NTLP-SD/ABLtest/"
-path_ran="/scratch365/tpeng2/NTLP-SD/ABLtest/u.le.cou000"
-path_histog="/scratch365/tpeng2/NTLP-SD/ABLtest/"
+path_seed="/scratch365/tpeng2/NTLP-SD/ABLevap/"
+path_part="/scratch365/tpeng2/NTLP-SD/ABLevap/part.le.cou000"
+path_res="/scratch365/tpeng2/NTLP-SD/ABLevap/u.le.cou000"
+path_sav="/scratch365/tpeng2/NTLP-SD/ABLevap/"
+path_his="/scratch365/tpeng2/NTLP-SD/ABLevap/"
+path_viz_xy="/scratch365/tpeng2/NTLP-SD/ABLevap/"
+path_viz_xz="/scratch365/tpeng2/NTLP-SD/ABLevap/"
+path_viz_yz="/scratch365/tpeng2/NTLP-SD/ABLevap/"
+path_stuf="/scratch365/tpeng2/NTLP-SD/ABLevap/"
+path_ran="/scratch365/tpeng2/NTLP-SD/ABLevap/u.le.cou000"
+path_histog="/scratch365/tpeng2/NTLP-SD/ABLevap/"
 /
 
 
@@ -111,13 +113,14 @@ Sc = 0.615
 Rd = 286.9     !J/kg-K  dry air gas constant
 
 !Particles:
-tnumpart = 0e5
-mult_init = 1e1
+tnumpart = 6e3
+mult_init = 1e3
 massfrac=0.05 !Before being multiped by mult_init
 !tnumpart = 131072
 !mult_init = 64e6
 rhow=1000.0  !kg/m^3
 part_grav = 0,0,-9.81
+Vpmax = 0.9
 Cpp = 4179.0  !J/kg-K
 Mw = 0.018015  !kg/mol
 Ru = 8.3144    !J/mol-K Universal gas constant
@@ -130,7 +133,7 @@ Os = 1.093
 !Particle initial conditions:
 radius_init=25e-6
 Tp_init = 300.0
-vp_init = 0.0, 0.0, 0.5
+vp_init = 0.0, 0.0, 1.0
 qf_init = 0.01
 
 /
